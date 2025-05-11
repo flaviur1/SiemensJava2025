4aa0cf59c3625bc501f114ac9c74f195cf427b84
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-05-11 23:24:07 +0300

    final commit, failed to do some tests

--- src/test/java/com/siemens/internship/InternshipApplicationTests.java
+++ src/test/java/com/siemens/internship/InternshipApplicationTests.java
@@ -1,13 +1,49 @@
 package com.siemens.internship;
 
+import com.siemens.internship.model.Item;
+import com.siemens.internship.repository.ItemRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class InternshipApplicationTests {
+import java.util.List;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class InternshipApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @BeforeEach
+    void cleanUp() {
+        itemRepository.deleteAll();
+    }
+
+    @Test
+    void createNewItem() {
+        Item item = new Item(null, "Test", "desc", "NEW", "test@example.com");
+
+        webTestClient.post().uri("/api/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(item)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("Test")
+                .jsonPath("$.email").isEqualTo("test@example.com");
+
+        List<Item> items = itemRepository.findAll();
+        assertThat(items).hasSize(1);
+    }
 
 }
